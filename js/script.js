79941e294618475693a0f32bf1aefd34ca0850d6
EvaXausa <xausa.eva@gmail.com>
2023-01-18 15:27:33 +0100

    file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 console.log("Vue ok", Vue);
 const app = Vue.createApp({
-  data() {},
+  name: "Carousel",
+  data() {
+    return {
+      currentIndex: 0,
+      pictures,
+    };
+  },
+  computed: {
+    lastPicturesIndex() {
+      return this.pictures.lenght - 1;
+    },
+  },
+  methods: {
+    goToPrev() {
+      if (this.currentIndex === 0) this.currentIndex = this.lastPicturesIndex;
+      else this.currentIndex--;
+    },
+    goToNext() {
+      if (this.currentIndex === this.lastPicturesIndex) this.currentIndex = 0;
+      else this.currentIndex++;
+    },
+    goTothis(index) {
+      this.currentIndex = index;
+    },
+  },
+  mounted() {
+    setInterval(this.goToNext, 2000);
+  },
 });
+app.mount("#root");
